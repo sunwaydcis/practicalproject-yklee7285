7a86e315ef587ff6803d1daf185aa76440b54ae4
Lee Yee Kang <75201065+yklee7285@users.noreply.github.com>
2025-06-30 17:52:08 +0800

    Changes made
    lab5

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,16 +1,36 @@
 package ch.makery.address
 
+import ch.makery.address.model.Person
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
+import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 
 object MainApp extends JFXApp3:
 
   //Window Root Pane
   var roots: Option[scalafx.scene.layout.BorderPane] = None
+  /**
+   * The data as an observable list of Persons.
+   */
+  val personData = new ObservableBuffer[Person]()
+
+  /**
+   * Constructor
+   */
+  personData += new Person("Hans", "Muster")
+  personData += new Person("Ruth", "Mueller")
+  personData += new Person("Heinz", "Kurz")
+  personData += new Person("Cornelia", "Meier")
+  personData += new Person("Werner", "Meyer")
+  personData += new Person("Lydia", "Kunz")
+  personData += new Person("Anna", "Best")
+  personData += new Person("Stefan", "Meier")
+  personData += new Person("Martin", "Mueller")
 
   override def start(): Unit =
     // transform path of RootLayout.fxml to URI for resource location.
@@ -37,3 +57,26 @@ object MainApp extends JFXApp3:
     loader.load()
     val roots = loader.getRoot[jfxs.layout.AnchorPane]
     this.roots.get.center = roots
+
+//    val stringA =  new StringProperty("Sunway") //publisher
+//    val stringB = new StringProperty("Monash") //subscriber
+//    val stringC = new StringProperty("Taylor") //subscriber
+//
+//    stringB.<==(stringA)
+//    stringC.<==(stringA)
+//    stringA.value= "Segi"
+//    stringC.value = "UTM"
+//
+//    println("String A value:" + stringA.value)
+//    println("String B value:" + stringB.value)
+//    println("String C value:" + stringC.value)
+//    stringA.onChange((a,b,c) => {
+//      println("a has changed value from" + b + "to" + c)
+//    })
+//    stringA.value = "Monash"
+//
+//    val add:(Int, Int) => Int = (a: Int, b: Int) => {a + b}
+//    println(add(1,2))
+
+
+
